0afb3099079c87b55cbc4c7e51c8ccaad3cb0804
svare <mon3mpk@gmail.com>
2024-12-21 17:12:48 +0200

    #2

--- project/src/components/js/login.js
+++ project/src/components/js/login.js
@@ -3,10 +3,14 @@ import styles from '../css/login.module.css'
 import netflix from '../../assets/net.png'
 
 function login() {
+
+      
+          
+    
  
   return(
-   
-    <div className={styles.container1}>
+      <div className={styles.page}>
+       <div className={styles.container1}>
           <div className={styles.navbar}>
                 <img className={styles.logo} src={netflix}></img>
                 <select className={styles.dropmenu} > <option value="en">English</option> <option value="ar">العربية</option> </select>
@@ -16,10 +20,25 @@ function login() {
            
                 <h1 className={styles.text}>Unlimited Movies, TV</h1>
                 <h1 className={styles.text2}>shows ,and more</h1>
-            
-          
+                <h1 className={styles.text3}>Starts at EGP 70. Cancel anytime.</h1>
+                <h1 className={styles.text4}>Ready to watch? Enter your email to create or restart your membership.</h1>
+          </div>
+
+          <div className={styles.midsection2}>
+
+          <input type="text" id="email-field"  placeholder="Email address" className={styles.box}></input>
+          <button className={styles.getstarted}>Get Started</button>
+         
 
           </div>
+      </div>
+       <div className={styles.container2}>
+
+      <h1>gftft</h1>
+
+    </div>
+
+
     </div>
    
 );
