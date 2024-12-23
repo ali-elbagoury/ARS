63f24e747336965730a651564f282d474f1c2f49
svare <mon3mpk@gmail.com>
2024-12-23 17:08:58 +0200

    react slider

--- project/src/components/js/card.js
+++ project/src/components/js/card.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import styles from '../css/login.module.css'
+import styles from '../css/card.module.css'
+import image from '../../assets/money-heist-netflix-144238.jpg'
 
 function card() {
   return (
-    <div>
-     
+    <div className={styles.contain}>
+
+    
+
+     <h1 className={styles.number}>1</h1>
+ 
+     <img src={image} className={styles.img}></img>
+
+
+
+    
+
     </div>
   )
 }
--- project/src/components/js/login.js
+++ project/src/components/js/login.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import styles from '../css/login.module.css';
 import netflix from '../../assets/net.png';
-import Card from '../js/card'
+import Card from './card'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 function Login() {
   const [email, setEmail] = useState('');
   const [isValid, setIsValid] = useState(true);
@@ -12,7 +15,27 @@ function Login() {
     const emailRegex = /^[^\s@]+@[^\s@(0-9)]+\.[^\s@]+$/;
     setIsValid(emailRegex.test(emailInput));
   };
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 3
+    }
+  };
 
+ 
   return (
     <div className={styles.page}>
       <div className={styles.container1}>
@@ -34,26 +57,38 @@ function Login() {
         </div>
 
         <div className={styles.midsection2}>
-          <input type="text" id="email-field"placeholder="Email address" className={styles.box} value={email} onChange={validateEmail} style={{ borderColor: isValid ? 'initial' : '#ad5456' }} />
+          <input type="text" id="email-field" placeholder="Email address" className={styles.box} value={email} onChange={validateEmail} style={{ borderColor: isValid ? 'initial' : '#ad5456' }} />
           <button className={styles.getstarted}>Get Started</button>
           {!isValid && (
-            <p id="email-error" style={{ color: '#ad5456',display:"block",position:"absolute",marginLeft:'590px',marginTop:'55px' }}>Invalid email address</p>
+            <p id="email-error" style={{ color: '#ad5456', display: "block", position: "absolute", marginLeft: '590px', marginTop: '55px' }}>Invalid email address</p>
           )}
         </div>
       </div>
       <div className={styles.container2}>
-                  <h1 className={styles.trend}>Trending Now</h1>
-                  <div className={styles.dropdown}>
-                  <select className={styles.dropmenu1}>
-                  <option value="en">Egypt</option>
-                  <option value="ar">Global</option>
-                  </select>
-                  <select className={styles.dropmenu2}>
-                  <option value="en">Movies</option>
-                  <option value="ar">TV Shows</option>
-                  </select>
-                  <Card/> <Card/> <Card/>
-                  </div>
+        <h1 className={styles.trend}>Trending Now</h1>
+        <div className={styles.dropdown}>
+          <select className={styles.dropmenu1}>
+            <option value="en">Egypt</option>
+            <option value="ar">Global</option>
+          </select>
+          <select className={styles.dropmenu2}>
+            <option value="en">Movies</option>
+            <option value="ar">TV Shows</option>
+          </select>
+        </div>
+        <div className={styles.carocontain}>
+        <Carousel responsive={responsive} className={styles.caro}>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+         
+       
+          
+    </Carousel>
+    </div>
       </div>
     </div>
   );
