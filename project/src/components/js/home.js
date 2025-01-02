9e4790406114dc4ce94b87974a71ff61099857ac
svare <mon3mpk@gmail.com>
2025-01-02 22:17:27 +0200

    homePage

--- project/src/components/js/home.js
+++ project/src/components/js/home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from '../css/home.module.css'
 import Poster from '../../assets/lacasa.jpg'
 import netflix from '../../assets/net.png';
+import search from '../../assets/search-icon.png'
+import user from '../../assets/user.jpg'
 
 function home() {
-  return (
-    <div>
-        <div className={styles.container}>
-      <div className={styles.nav}>
+  return (<div className={styles.page}>
+     <div className={styles.container1}>
+        <div className={styles.container2}>
+          <div className={styles.nav}>
         <img src={netflix} className={styles.logo}></img>
         <ul className={styles.navtext}>
         <li className={styles.text}>Home</li>
@@ -15,13 +17,23 @@ function home() {
         <li className={styles.text}>Films</li>
         <li className={styles.text}>Recently Added</li>
         <li className={styles.text}>My List</li>
-
         </ul>
+            <div className={styles.icons}>
+                <img src={search} className={styles.search}></img>
+                <img src={user} className={styles.user}></img>
+             </div>
 
-      </div>
+          </div>
+      
 
-      </div>
+        </div>
+     
+      
+
     </div>
+    </div>
+      
+  
   )
 }
 
