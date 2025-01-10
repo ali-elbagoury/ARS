c26a28f9b9510aace4d870695f8a8e6ac258b671
svare <mon3mpk@gmail.com>
2025-01-10 04:07:40 +0200

    Update App.js

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,11 @@
 import Login from "../src/components/js/login"
 import Home from "./components/js/home.js"
+import SignIn from "./components/js/signin.js"
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Login/>
     
     </div>
   );
