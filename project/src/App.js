81e714d90c93eaeea0052e7ead0197af8a4ed2af
svare <mon3mpk@gmail.com>
2025-01-13 02:36:43 +0200

    update

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,12 @@
 import Login from "../src/components/js/login"
 import Home from "./components/js/home.js"
+import UserPage from './components/js/userpage.js'
 
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <UserPage/>
     
     </div>
   );
--- project/src/components/js/userpage.js
+++ project/src/components/js/userpage.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import styles from '../css/user.module.css'
+import styles from '../css/user.module.css';
+import user1 from '../../assets/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg'
+import user2 from '../../assets/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg'
+import user3 from '../../assets/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg'
+
+
 
 function userpage() {
   return (
     <div className={styles.container}>
         <div className={styles.user}>
-
+        <img src={user1}></img>
+        <img src={user2}></img>
+        <img src={user3}></img>
 
         </div>
       
